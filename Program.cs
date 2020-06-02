804e950adf3b7c808f461a2b17faf03d4825e6ab
info@yazilimfikirleri.com <info@yazilimfikirleri.com>
2020-06-02 14:28:45 +0300

    proje oluşturuldu.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Operatörler4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Artırma azaltma operatörleri
+            int sayi1;
+            int sayi2;
+            //sayi1 = 9;
+
+            //sayi2 = sayi1 + 1;
+            //Console.WriteLine(sayi2);
+            ////Console.Read();
+
+            //sayi1++;
+            //Console.WriteLine(sayi1);
+
+
+            ////Console.WriteLine("Sayı1'in değeri: "+sayi1);
+            ////Console.WriteLine("Sayı2'nin değeri: "+sayi2);
+
+            //Console.WriteLine(sayi1 + sayi2);
+
+            ////Console.WriteLine("Toplam sonuç " + sayi1+sayi2);
+            //Console.WriteLine("Toplam sonuç: " + (sayi1 + sayi2));
+
+            //sayi1 = 10;
+            //sayi2 = sayi1;
+
+            //sayi2 += 10;
+            //sayi2 *= 10;
+            //sayi2 -= 10;
+            //sayi2 /= 10;
+
+            //Console.WriteLine(sayi2);
+
+            sayi1 = 10;
+            sayi2 = 5;
+            string sonuc;
+            sonuc = "Toplam= ";
+
+            Console.WriteLine(sonuc + (sayi1 + sayi2));
+
+            //Bir öğrencinin vizeden almış olduğu puan 60, finalden almış olduğu puan ise 85'dir.
+            //Vizenin %30'u, final'in %70'ni alarak çıkan puanı kullanıcıya gösterin.
+
+            Console.Read();
+        }
+    }
+}
